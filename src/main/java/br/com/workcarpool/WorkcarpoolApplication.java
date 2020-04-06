6be20e4797cb958b867c097f28b4360e789790d6
Lúcio Leandro <lucio.leo85@gmail.com>
2020-04-06 19:55:14 -0300

    Uploading project

---
+++ src/main/java/br/com/workcarpool/WorkcarpoolApplication.java
@@ -0,0 +1,13 @@
+package br.com.workcarpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkcarpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkcarpoolApplication.class, args);
+	}
+
+}
